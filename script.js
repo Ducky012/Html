8a7c3efad07788fc124f2611e9bf99b8117cdabf
Ducky012 <96600847+Ducky012@users.noreply.github.com>
2022-11-01 13:31:58 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,4 @@
 function myFunction(){
 	document.getElementById("Demo").innerHTML= "What????????";
 }
-function Dates(){
-	const Date = new date();
-	document.getElementById("Demo").innerHTML = Date();
-}
 	
