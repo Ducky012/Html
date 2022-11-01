86b5b2dae0c4f7cf0cdca0571e7200e8aae61268
Ducky012 <96600847+Ducky012@users.noreply.github.com>
2022-11-01 12:25:37 -0700

    Update script.js

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ function myFunction(){
 	document.getElementById("Demo").innerHTML= "What????????";
 }
 function Date(){
-	const Date = new date();
-	document.getElementById("Demo").innerHTML = Date;
+	let Date = new date();
+	const Month = ["Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"]
+	document.getElementById("Demo").innerHTML = Month=[Date.getMonth()] + Date.getDate() + Date.getFullYear();
 }
